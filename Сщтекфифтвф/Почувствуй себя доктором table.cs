9634d4d3d48caefc0449f7e23bd28563b88a68d9
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2023-01-30 14:33:54 +0300

    Add files via upload

---
+++ Сщтекфифтвф/Почувствуй себя доктором table.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Таблица
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Содание шапки
+            Table1.Columns.Add("Name", "Имя");
+            Table1.Columns.Add("Height", "Рост");
+            Table1.Columns.Add("Weight", "Вес");
+            Table1.Columns.Add("Group", "Группа крови");
+        }
+        bool flag = false;//отслеживает была ли нажата кнопка дважды. На второе нажатие останавливает таймер 
+        private void StartEnter_Click(object sender, EventArgs e)
+        {
+            if (!flag)
+            {
+                timer1.Start();
+                flag = true;
+            }
+            else
+            {
+                timer1.Stop();
+                flag= false;
+            }
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            string[] Name = { "Иван", "Василий", "Джордж","Полина"};
+            string[] GroupBload = { "A", "B", "AB" };
+            //Закидываем рандомные значения 
+            Table1.Rows.Add(Name[random.Next(0, 4)], random.Next(160, 200), random.Next(60, 120), GroupBload[random.Next(0, 3)]);
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            using (System.IO.StreamWriter Popka = new System.IO.StreamWriter("Cake.txt", false))//открываем файл для записи
+            {               
+                for(int str=0;str<Table1.Rows.Count-1;str++)//Сичтываем кол-во строк-1, тк последняя пустая
+                {
+                    for(int col=0;col<Table1.Columns.Count;col++)//перебираем столбцы
+                    {
+                        Popka.Write(Table1[col, str].Value);//записываем в файл слово,НО не переходим на другую строку
+                        if(col!=Table1.Columns.Count-1)
+                        {
+                            Popka.Write(";");//Если записываемый элемент, не группа корови, то после него ставим ;
+                        }
+                            
+                    }
+                    Popka.WriteLine();//переходим на следующую строку
+                }
+
+            }
+
+        }
+
+        private void Loaging_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog()==DialogResult.OK)//Вызываем окно выбора файла, если нажали ОК то продолжаем
+            {
+                Table1.Rows.Clear();//очищаем строки
+                string []str = File.ReadAllLines(openFile.FileName);//Записываем в массив строк строки из файла, в скобках указывается имя файла
+                for(int i = 0; i < str.Length;i++)//пробигаем по всем считынным строкам
+                {
+                    //буферы в которые мы будем помещать значения Имен, Роста, Веса и группы крови для рассматриваемой строки
+                    List<char> BufName = new List<char>();
+                    List<char> BufHeight = new List<char>();
+                    List<char> BufWight = new List<char>();
+                    List<char> BufGroup = new List<char>();
+                    int pole = 0;//Характеризуем какой элемент мы считываем посимвольно 
+                    for( int c=0; c < str[i].Length;c++)
+                    {
+                        char sim=str[i][c];
+                        if (sim != ';')
+                        {
+                            if (pole == 0)
+                                BufName.Add(sim);//Помещаем в бурферы
+                            if (pole == 1)
+                                BufHeight.Add(sim);
+                            if (pole == 2)
+                                BufWight.Add(sim);
+                            if (pole == 3)
+                                BufGroup.Add(sim);
+                        }
+                        else pole++;//Если вдруг попался символ ; то начит переходим к считыванию следующего элемента
+                    }
+                    //Buf.ToArray() возвращает массив типа char, с помощью String.Concat(...) вы склеиваем массив char в одну строку
+                    Table1.Rows.Add(String.Concat(BufName.ToArray()),String.Concat(BufHeight.ToArray()),String.Concat(BufWight.ToArray()),String.Concat( BufGroup.ToArray()));
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ Сщтекфифтвф/СМЕШАРИКИ АХАХА.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Шарики
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            Speed.Text = "50";
+            timer1.Interval = 50;
+            CountTap.Text = ($"Количество нажатий: {CountTapInt}");
+        }
+        class Ball// класс шараl
+        {
+            public int x0;
+            public int y0;
+            public int Radius = 6;
+            public Brush color = new SolidBrush(Color.White);
+            public Ball(int X, int Y, Brush color)
+            {
+                this.x0 = X;
+                this.y0 = Y;
+                this.color = color;
+            }
+
+        }
+        List<Ball> Balls = new List<Ball>();//список шаров
+        int CountTapInt = 0;//Количество нажатий 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            panel1.BackColor = Color.White;
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Random rnd = new Random();
+            Brush brush = new SolidBrush(Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(255)));
+            g.FillEllipse(brush, e.X - 6, e.Y - 6, 12, 12);
+            Balls.Add(new Ball(e.X, e.Y, brush));
+            CountTapInt++;
+            CountTap.Text = ($"Количество нажатий: {CountTapInt}");//Количество нажатий 
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            Graphics g = panel1.CreateGraphics();
+            for (int i = 0; i < Balls.Count; i++)//перебор всех шаров
+            { 
+                bool flag = true;//проверка, если true неободимо создавать новый шар, если false, то нет
+                Balls[i].Radius++;//увеличиваю радиус
+                //проверка на соприкосновение с границей panel
+                if (Balls[i].x0 + Balls[i].Radius > panel1.Width || Balls[i].x0 - Balls[i].Radius < 0 || Balls[i].y0 + Balls[i].Radius > panel1.Height || Balls[i].y0 - Balls[i].Radius < 0)
+                {
+                    g.FillEllipse(Brushes.White, Balls[i].x0 - Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[i].Radius * 2, Balls[i].Radius * 2);
+                    Balls.RemoveAt(i);
+                    flag = false;
+                }
+                //если шар не коснулся границы panel, проверим на прикосновение к другим шарам
+                else
+                {
+                    for (int j = 0; j < Balls.Count; j++)//перебираем каждый i шар с остальными 
+                    {
+                        if (i != j)
+                        {
+                            //проверка находится ли левый нижний угол в огибающем квадрате j куба
+                            bool left_down = Oblost(Balls[i].x0 - Balls[i].Radius, Balls[i].y0 + Balls[i].Radius, Balls[j]);
+                            //проверка правого нижнего
+                            bool right_down = Oblost(Balls[i].x0 + Balls[i].Radius, Balls[i].y0 + Balls[i].Radius, Balls[j]);
+                            //левого верхнего
+                            bool left_up = Oblost(Balls[i].x0 - Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[j]);
+                            // правого верхнего
+                            bool right_up = Oblost(Balls[i].x0 + Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[j]);
+                                       //if (Oblost(Balls[i].x0 - Balls[i].Radius, Balls[i].y0 + Balls[i].Radius, Balls[j]) || Oblost(Balls[i].x0 + Balls[i].Radius, Balls[i].y0 + Balls[i].Radius, Balls[j]) || Oblost(Balls[i].x0 - Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[j]) || Oblost(Balls[i].x0 + Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[j]))
+                            //далее проверка, если хотя бы один угол находится вне огибающего квадрата, то значит они пересеклись. Может 
+                            //быть ситуалция когда один шар находится в другом и тогда их огибащие квадрате не пересекутсься
+                            if (left_down != right_down) flag = false;
+                            if (left_down != right_up) flag = false;
+                            if (left_down != left_up) flag = false;
+                            if (right_down != left_up) flag = false;
+                            if (right_down != right_down) flag = false;
+                            if (left_up != right_up) flag = false;
+                            //если шары пересеклись значит стераем их и убираем из массива
+                            if (!flag)
+                            {
+                                g.FillEllipse(Brushes.White, Balls[j].x0 - Balls[j].Radius, Balls[j].y0 - Balls[j].Radius, Balls[j].Radius * 2, Balls[j].Radius * 2);
+                                g.FillEllipse(Brushes.White, Balls[i].x0 - Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[i].Radius * 2, Balls[i].Radius * 2);
+                                flag = false;
+                                //т.к не наем какой из них идет раньше а какой позже, что бы не обращаться к несуществующему числу делаем проверку
+
+                                if (i < j)
+                                {
+                                    Balls.RemoveAt(j);
+                                    Balls.RemoveAt(i);
+                                    break;
+                                }
+                                else
+                                {
+                                    Balls.RemoveAt(i);
+                                    Balls.RemoveAt(j);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (flag)//если квадрат ничего не задел но рисуем его
+                {
+                    g.FillEllipse(Balls[i].color, Balls[i].x0 - Balls[i].Radius, Balls[i].y0 - Balls[i].Radius, Balls[i].Radius * 2, Balls[i].Radius * 2);
+                }
+            }
+        }
+        private bool Oblost(int x, int y, Ball ball)//проверка точкy на вхождение ее в описывающий квадрат другого круга 
+        {
+            if ((x < ball.x0 + ball.Radius && x > ball.x0 - ball.Radius) && (y < ball.y0 + ball.Radius && y > ball.y0 - ball.Radius))
+                return true;
+                      //if ((x == ball.x0 + ball.Radius || x == ball.x0 - ball.Radius) && (y < ball.y0 + ball.Radius && y > ball.y0 - ball.Radius))
+                     //{
+                    //    return true;
+                   //}
+                  //if ((y == ball.y0 + ball.Radius || y == ball.y0 - ball.Radius) && (x < ball.x0 + ball.Radius && x > ball.x0 - ball.Radius))
+                 //{
+                //    return true;
+               //}
+            return false;
+        }
+        private void Apply_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(Speed.Text, out int speed))
+            {
+                if (int.Parse(Speed.Text) > 100 || int.Parse(Speed.Text) <= 0)
+                {
+                    MessageBox.Show("Максимальная скороть 100");
+                }
+                else { timer1.Interval =( 101 - int.Parse(Speed.Text)); }
+            }
+        }
+    }
+}
